8b983dd0ed78afa62430ad57b0d9b9996daa98aa
Jordan Pitlor <jordan.pitlor@avasure.com>
2021-11-11 15:51:39 -0500

    PEMDAS and more printing tricks

--- Python Fundamentals/Intro/More Printing Tricks/main.py
+++ Python Fundamentals/Intro/More Printing Tricks/main.py
@@ -1,3 +1,10 @@
-if __name__ == "__main__":
-    # Write your solution here
-    pass
+# Additional Parameters
+print("Hello,", "I", "am", "John")
+print(1, 2, 3, 4)
+
+# Concatenate Operator
+print("I am" + "8" + "years old")
+print("Hello" + "John")
+
+# Fix me!
+print("I am " + "7" + " years old")
--- Python Fundamentals/Intro/PEMDAS/main.py
+++ Python Fundamentals/Intro/PEMDAS/main.py
@@ -1,3 +1,8 @@
-if __name__ == "__main__":
-    # Write your solution here
-    pass
+problem1 = 100 - 4 * (7 - 4) ** 3
+print(str(problem1))
+
+problem2 = 20 + 3 * (5 - 1)
+print(str(problem2))
+
+problem3 = 3 + 2 ** 2 * (1 + 8)
+print(str(problem3))
